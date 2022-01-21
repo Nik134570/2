9580524c647f41ff66104b07a7e257193a9facc3
NIk134 <92600545+Nik134570@users.noreply.github.com>
2022-01-22 00:09:52 +0400

    Update game.py

--- game.py
+++ game.py
@@ -3,6 +3,10 @@ import sys
 
 import pygame
 
+image = pygame.image.load('21.jpg')
+image1 = pygame.image.load('39.png')
+image2 = pygame.image.load('56.png')
+
 class Board:
     colors = ["black", "white"]
     def __init__(self, width, height):
@@ -11,21 +15,35 @@ class Board:
         self.board = [[0] * width for _ in range(height)]
         self.left = 10
         self.top = 10
-        self.size = 100
+        self.size = 50
 
-    def render(self, screen):
+    def render(self, screen, d):
+        if d == 1:
+            dog_surf = pygame.transform.scale(image, (width, height))
+            dog_rect = dog_surf.get_rect(bottomright=(width, height))
+            screen.blit(dog_surf, dog_rect)
+        else:
+            pygame.draw.rect(screen, (0, 0, 0),
+                                     (0, 0, width, height), 0)
         for j in range(0, self.width):
             for i in range(0, self.height):
-                if self.board[i][j] == 1:
-                    pygame.draw.rect(screen, (255, 0, 0),
-                                    (10 + i * self.size, 10 + j * self.size, self.size, self.size), 0)
-                    pygame.draw.rect(screen, (255, 255, 255),
-                                     (10 + i * self.size, 10 + j * self.size, self.size, self.size), 1)
-                elif self.board[i][j] == 0:
+                if self.board[i][j] == 1 and d == 0:
                     pygame.draw.rect(screen, (0, 0, 0),
-                                     (10 + i * self.size, 10 + j * self.size, self.size, self.size), 0)
+                                     (i * self.size, j * self.size, self.size, self.size), 1)
+                    dog_surf1 = pygame.transform.scale(image1, (48, 48))
+                    dog_surf1.set_colorkey((255, 255, 255))
+                    dog_rect1 = dog_surf1.get_rect(bottomright=(i * self.size + 49, j * self.size + 49))
+                    screen.blit(dog_surf1, dog_rect1)
+                elif self.board[i][j] == 0:
                     pygame.draw.rect(screen, (255, 255, 255),
-                                     (10 + i * self.size, 10 + j * self.size, self.size, self.size), 1)
+                                     (i * self.size, j * self.size, self.size, self.size), 1)
+                else:
+                    pygame.draw.rect(screen, (0, 0, 0),
+                                     (i * self.size, j * self.size, self.size, self.size), 1)
+                    dog_surf1 = pygame.transform.scale(image2, (48, 48))
+                    dog_surf1.set_colorkey((255, 255, 255))
+                    dog_rect1 = dog_surf1.get_rect(bottomright=(i * self.size + 49, j * self.size + 49))
+                    screen.blit(dog_surf1, dog_rect1)
         
                     
 
@@ -52,6 +70,11 @@ class Board:
                 self.board[i][j] = 0
         self.board[x][y] = (self.board[x][y] + 1) % 2
 
+    def n(self):
+        for j in range(0, self.width):
+            for i in range(0, self.height):
+                self.board[i][j] = 0
+
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
@@ -60,9 +83,10 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
+
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 800, 553
+    size = width, height =1800, 1000
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Game over")
     running = True
@@ -75,34 +99,30 @@ if __name__ == '__main__':
     sprite.rect = sprite.image.get_rect()
     sprite.rect.x = -600
     sprite.rect.y = 0
-    size = width, height = 800, 553
     screen = pygame.display.set_mode(size)
     pygame.display.flip()
-    board = Board(5, 7)
+    board = Board(height // 50, width // 50)
     running = True
     d = 1
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN and d == 1:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 board.get_click(event.pos)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
-            d = 0
+            running = False
         screen.fill((0, 0, 0))
-        board.render(screen)
+        board.render(screen, 0)
         pygame.display.flip()
     running = True
-    while running:
-        screen.fill((255, 255, 255))
-        all_sprites.draw(screen)
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+    while running == True:
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == MYEVENTTYPE:
-                sprite.rect.x += 2
-                sprite.rect.move(sprite.rect.x, 0)
-            if sprite.rect.x >= 0:
-                pygame.time.set_timer(MYEVENTTYPE, 0)
+        board.render(screen, 1)
         pygame.display.flip()
