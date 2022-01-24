1e420c348d412473bbf6d52e2b3dc8c720a8f8de
NIk134 <92600545+Nik134570@users.noreply.github.com>
2022-01-24 23:06:51 +0400

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,11 @@
 import os
 from re import S
 import sys
+import cv2
 
 import pygame
+
+agh = [[1, 2], [2, 3]]
 
 image = pygame.image.load('D:/21.jpg')
 image1 = pygame.image.load('D:/39.png')
@@ -15,6 +18,27 @@ image7 = pygame.image.load('D:/ук.png')
 image8 = pygame.image.load('D:/крылья.png')
 
 all_sprites = pygame.sprite.Group()
+
+
+def cv2ImageToSurface(cv2Image):
+    size = cv2Image.shape[1::-1]
+    format = 'RGBA' if cv2Image.shape[2] == 4 else 'RGB'
+    cv2Image[:, :, [0, 2]] = cv2Image[:, :, [2, 0]]
+    surface = pygame.image.frombuffer(cv2Image.flatten(), size, format)
+    return surface.convert_alpha() if format == 'RGBA' else surface.convert()
+
+def loadGIF(filename):
+    gif = cv2.VideoCapture(filename)
+    frames = []
+    while True:
+        ret, cv2Image = gif.read()
+        if not ret:
+            break
+        pygameImage = cv2ImageToSurface(cv2Image)
+        frames.append(pygameImage)
+    return frames
+
+
 
 
 class Board:
@@ -87,6 +111,10 @@ class Board:
                                      (0, 0, width, height), 0)
         for j in range(0, self.width):
             for i in range(0, self.height):
+                pygame.draw.rect(screen, (255, 255, 255),
+                                     (i * self.size, j * self.size, self.size, self.size), 1)
+        for j in range(0, self.width):
+            for i in range(0, self.height):
                 if self.board[i][j] == 1 and d == 0:
                     s = 1
                     pygame.draw.rect(screen, (0, 0, 0),
@@ -98,7 +126,7 @@ class Board:
                 elif self.board[i][j] == 0:
                     pygame.draw.rect(screen, (255, 255, 255),
                                      (i * self.size, j * self.size, self.size, self.size), 1)
-                elif self.board[i][j] == 1 and d == 1:
+                elif self.board[i][j] == 1 and d >= 1:
                     pygame.draw.rect(screen, (0, 0, 0),
                                      (i * self.size, j * self.size, self.size, self.size), 1)
                     dog_surf1 = pygame.transform.scale(image2, (68, 68))
@@ -126,6 +154,19 @@ class Board:
                     pygame.draw.line(screen, (0, 0, 255), (i * self.size + 1, j * self.size + 69), (i * self.size + 69, j * self.size + 1), 5)
                     pygame.draw.rect(screen, (255, 255, 255),
                                      (i * self.size, j * self.size, self.size, self.size), 1)
+        if d == 2:
+            global agh
+            for t in agh:
+                i = t[0]
+                j = t[1]
+                pygame.draw.rect(screen, (0, 0, 0),
+                                     (i * self.size, j * self.size, self.size, self.size), 1)
+                dog_surf1 = pygame.transform.scale(image3, (38, 38))
+                dog_surf1.set_colorkey((255, 255, 255))
+                dog_rect1 = dog_surf1.get_rect(bottomright=(i * self.size + 53, j * self.size + 53))
+                screen.blit(dog_surf1, dog_rect1)
+                pygame.draw.rect(screen, (255, 255, 255),
+                                 (i * self.size, j * self.size, self.size, self.size), 1)
         if d == 1:
             dog_surf10 = pygame.transform.scale(image4, (420, 210))
             dog_rect10 = dog_surf10.get_rect(bottomright=(23 * self.size - 35, 8 * self.size))
@@ -240,37 +281,26 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
-class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, sheet, columns, rows, x, y):
-        super().__init__(all_sprites)
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
-
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns, 
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
-
-    def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        self.image = self.frames[self.cur_frame]
 
 if __name__ == '__main__':
     def stat(x1, y1, p):
-        for i in p:
-            if i[0] == x1 and i[1] == y1:
-                global coins
-                coins += 1
-                print(coins)
-                n = p.index(i)
-                del p[n]
+        global d
+        global agh
+        global coins
+        if d == 1:
+            for i in p:
+                if i[0] == x1 and i[1] == y1:
+                    coins += 1
+                    print(coins)
+                    n = p.index(i)
+                    del p[n]
+        if d == 2:
+            for t in agh:
+                if t[0] == x1 and t[1] == y1:
+                    coins += 1
+                    print(coins)
+                    n = agh.index(t)
+                    del agh[n]
     coins = 0
     pygame.init()
     size = width, height = 1820, 980
@@ -436,20 +466,163 @@ if __name__ == '__main__':
             screen.blit(text, (text_x, text_y))
         pygame.display.flip()
     running = True
-    dragon = AnimatedSprite(load_image("D:/dragon_sheet8x2.png"), 8, 2, 50, 50)
-    all_sprites.add(dragon)
     clock = pygame.time.Clock()
-    MYEVENTTYPE = pygame.USEREVENT + 1
-    pygame.time.set_timer(MYEVENTTYPE, 10)
+    gifFrameList = loadGIF(r"D:/img3.gif")
+    currentFrame = 0
+    timer = pygame.time.get_ticks
+    timeout = 7000 # milliseconds
+    deadline = timer() + timeout
     while running == True:
-        screen.fill((255, 255, 255))
+        now = timer()
+        clock.tick(25)
+        screen.fill((0, 0, 0))
         all_sprites.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == MYEVENTTYPE:
-                dragon.update()
-                pygame.display.flip()
-        clock.tick(10)
+        if now > deadline:
+            running = False
+        rect = gifFrameList[currentFrame].get_rect(center = (800, 400))
+        b = pygame.transform.scale(gifFrameList[currentFrame], (400, 400))
+        screen.blit(b, rect)
+        currentFrame = (currentFrame + 1) % len(gifFrameList)
         pygame.display.flip()
-    pygame.quit()
+    running = True
+    d = 2
+    while running == True:
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s and y1 != height // 70 - 1 and gh == 0:
+                    board.fm(x1, y1)
+                    y1 += 1
+                    stat(x1, y1, p)
+                if event.key == pygame.K_w and y1 != 0 and gh == 0:
+                    board.fp(x1, y1)
+                    y1 -= 1
+                    stat(x1, y1, p)
+                if event.key == pygame.K_d and x1 != width // 70 - 1 and gh == 0:
+                    board.rm(x1, y1)
+                    x1 += 1
+                    stat(x1, y1, p)
+                if event.key == pygame.K_a and x1 != 0 and gh == 0:
+                    board.rp(x1, y1)
+                    x1 -= 1
+                    stat(x1, y1, p)
+                if event.key == pygame.K_m:
+                    gh = (gh + 1) % 2
+                if event.key == pygame.K_j and x1 == 19 and y1 == 6:
+                    running = False
+                print(x1, y1)
+                #19 6
+        board.render(screen, 2)
+        if(gh == 1):
+            a = width
+            b = height
+            pygame.draw.rect(screen, (255, 255, 255), (30, 160,
+                                           440, 440), 0)
+            pygame.draw.rect(screen, (255, 255, 255), (1090, 20,
+                                           440, 440), 0)
+            pygame.draw.rect(screen, (255, 255, 255), (590, 430,
+                                           440, 440), 0)
+            pygame.draw.rect(screen, (0, 255, 0), (30, 160,
+                                           440, 440), 5)
+            pygame.draw.rect(screen, (0, 255, 0), (1090, 20,
+                                           440, 440), 5)
+            pygame.draw.rect(screen, (0, 255, 0), (590, 430,
+                                           440, 440), 5)
+            pygame.draw.rect(screen, (255, 255, 255), (1350, 500,
+                                           440, 440), 0)
+            pygame.draw.rect(screen, (0, 255, 0), (1350, 500,
+                                           440, 440), 5)
+
+            pygame.draw.rect(screen, (0, 0, 0), (50, 180,
+                                           400, 400), 0)
+            pygame.draw.rect(screen, (0, 0, 0), (1110, 40,
+                                           400, 400), 0)
+            pygame.draw.rect(screen, (0, 0, 0), (610, 450,
+                                           400, 400), 0)
+            pygame.draw.rect(screen, (0, 255, 0), (50, 180,
+                                           400, 400), 3)
+            pygame.draw.rect(screen, (0, 255, 0), (1110, 40,
+                                           400, 400), 3)
+            pygame.draw.rect(screen, (0, 255, 0), (610, 450,
+                                           400, 400), 3)
+            pygame.draw.rect(screen, (0, 0, 0), (1370, 520,
+                                           400, 400), 0)
+            pygame.draw.rect(screen, (0, 255, 0), (1370, 520,
+                                           400, 400), 3)
+
+            pygame.draw.rect(screen, (0, 0, 0), (600, 80,
+                                           400, 300), 0)
+            pygame.draw.rect(screen, (0, 255, 0), (600, 80,
+                                           400, 300), 5)
+            pygame.draw.rect(screen, (0, 0, 0), (50, 670,
+                                           400, 200), 0)
+            pygame.draw.rect(screen, (0, 255, 0), (50, 670,
+                                           400, 200), 5)
+            pygame.draw.rect(screen, (0, 0, 0), (1075, 520,
+                                           250, 400), 0)
+            pygame.draw.rect(screen, (0, 255, 0), (1075, 520,
+                                           250, 400), 5)
+
+            pygame.draw.rect(screen, (0, 0, 0), (1600, 100,
+                                           140, 200), 0)
+            pygame.draw.rect(screen, (0, 255, 0), (1600, 100,
+                                           140, 200), 5)
+            dog_surf111 = pygame.transform.scale(image3, (70, 70))
+            dog_rect111 = dog_surf111.get_rect(bottomright=(1705, 205))
+            screen.blit(dog_surf111, dog_rect111)
+            font = pygame.font.Font(None, 40)
+            text = font.render("Coins:", True, (255, 255, 0))
+            text_x = 1672 - text.get_width() // 2
+            text_y = 230 - text.get_height() // 2
+            text_w = text.get_width()
+            text_h = text.get_height()
+            screen.blit(text, (text_x, text_y))
+            font = pygame.font.Font(None, 35)
+            text = font.render(str(coins), True, (255, 255, 0))
+            text_x = 1672 - text.get_width() // 2
+            text_y = 260 - text.get_height() // 2
+            text_w = text.get_width()
+            text_h = text.get_height()
+            screen.blit(text, (text_x, text_y))
+            
+            font = pygame.font.Font(None, 40)
+            text = font.render("Your Stats:", True, (0, 255, 0))
+            text_x = 1200 - text.get_width() // 2
+            text_y = 550 - text.get_height() // 2
+            text_w = text.get_width()
+            text_h = text.get_height()
+            screen.blit(text, (text_x, text_y))
+            dog_surf11 = pygame.transform.scale(image6, (300, 300))
+            dog_rect11 = dog_surf11.get_rect(bottomright=(400, 515))
+            screen.blit(dog_surf11, dog_rect11)
+            dog_surf13 = pygame.transform.scale(image5, (300, 300))
+            dog_rect13 = dog_surf13.get_rect(bottomright=(960, 790))
+            screen.blit(dog_surf13, dog_rect13)
+            dog_surf15 = pygame.transform.scale(image7, (350, 350))
+            dog_rect15 = dog_surf15.get_rect(bottomright=(1735, 895))
+            screen.blit(dog_surf15, dog_rect15)
+            dog_surf16 = pygame.transform.scale(image8, (300, 300))
+            dog_rect16 = dog_surf16.get_rect(bottomright=(1463, 385))
+            screen.blit(dog_surf16, dog_rect16)
+            font = pygame.font.Font(None, 70)
+            text = font.render("Your Profile:", True, (100, 255, 100))
+            text_x = 800 - text.get_width() // 2
+            text_y = 220 - text.get_height() // 2
+            text_w = text.get_width()
+            text_h = text.get_height()
+            screen.blit(text, (text_x, text_y))
+            pygame.draw.rect(screen, (0, 255, 0), (text_x - 20, text_y - 80,
+                                                   text_w + 40, text_h + 160), 3)
+            font = pygame.font.Font(None, 30)
+            text = font.render("press 'm' to return to the game", True, (255, 255, 255))
+            text_x = 250 - text.get_width() // 2
+            text_y = 770 - text.get_height() // 2
+            text_w = text.get_width()
+            text_h = text.get_height()
+            screen.blit(text, (text_x, text_y))
+        pygame.display.flip()
